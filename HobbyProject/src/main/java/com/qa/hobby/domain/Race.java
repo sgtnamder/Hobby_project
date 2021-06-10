92127d5e12f97c3ff3481327a6b6aa72c3e5d8db
sgtnamder <rredman@qa.com>
2021-06-10 13:05:33 +0100

    hp-11 #comment basic structure present

---
+++ HobbyProject/src/main/java/com/qa/hobby/domain/Race.java
@@ -0,0 +1,5 @@
+package com.qa.hobby.domain;
+
+public class Race {
+
+}
---
+++ HobbyProject/src/test/java/com/qa/hobby/HobbyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.hobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

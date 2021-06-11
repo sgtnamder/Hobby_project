23991e665547036e23748e7b82d8092e1ee274c5
sgtnamder <rredman@qa.com>
2021-06-11 12:04:37 +0100

    HP-15 HP-3 #comment RR: added update race to the rest controller with '/update'

--- HobbyProject/src/main/java/com/qa/hobby/controller/RaceController.java
+++ HobbyProject/src/main/java/com/qa/hobby/controller/RaceController.java
@@ -3,6 +3,7 @@ package com.qa.hobby.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,22 +15,28 @@ import com.qa.hobby.service.RaceService;
 @RestController
 @RequestMapping("/race")
 public class RaceController {
-	
+
 	private RaceService service;
-	
+
 	@Autowired
 	public RaceController(RaceService service) {
 		super();
-		this.service=service;
+		this.service = service;
 	}
+
 	@RequestMapping("/add")
 	public RaceDTO addRace(@RequestBody Race race) {
 		return this.service.addRace(race);
 	}
-	
+
 	@RequestMapping("/")
 	public List<RaceDTO> getAllRaces(){
 		return this.service.getAllRaces();
+	}
+
+	@RequestMapping("/update/{id}")
+	public RaceDTO updateRace(@RequestBody Race race, @PathVariable Integer id) {
+		return this.service.updateRace(id, race);
 	}
 	
 }

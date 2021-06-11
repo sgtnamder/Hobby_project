a9ad741e043f9b77845d0af7e5479cd0dd6d1945
sgtnamder <rredman@qa.com>
2021-06-11 11:42:54 +0100

    HP-14 HP-6 #comment RR: added the rest controller to read all driuvers from the DB using '/'

--- HobbyProject/src/main/java/com/qa/hobby/controller/DriverController.java
+++ HobbyProject/src/main/java/com/qa/hobby/controller/DriverController.java
@@ -1,5 +1,7 @@
 package com.qa.hobby.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,11 @@ public class DriverController {
 	public DriverDTO addDriver(@RequestBody Driver driver) {
 		return this.service.addDriver(driver);
 	}
+	
+	@RequestMapping("/")
+	public List<DriverDTO> getAllDrivers() {
+		return this.service.getAllDrivers();
+	}
+	
 	
 }

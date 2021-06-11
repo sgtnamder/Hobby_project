418bb05d5e356d2b57744a151f1a37c9ef594865
sgtnamder <rredman@qa.com>
2021-06-11 11:35:21 +0100

    HP-5 HP-14 #comment RR: added the abilty to add drivers throught a rest controller

--- HobbyProject/src/main/java/com/qa/hobby/controller/DriverController.java
+++ HobbyProject/src/main/java/com/qa/hobby/controller/DriverController.java
@@ -1,5 +1,29 @@
 package com.qa.hobby.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.hobby.domain.Driver;
+import com.qa.hobby.dto.DriverDTO;
+import com.qa.hobby.service.DriverService;
+
+@RestController
+@RequestMapping("/drivers")
 public class DriverController {
 
+	private DriverService service;
+	
+	@Autowired
+	public DriverController(DriverService service) {
+	super();
+	this.service = service;
+	}
+	
+	@RequestMapping("/add")
+	public DriverDTO addDriver(@RequestBody Driver driver) {
+		return this.service.addDriver(driver);
+	}
+	
 }
